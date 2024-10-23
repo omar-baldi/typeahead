5265db1910b397c2cfe6d3728adcb88516e6e231
Omar Baldi <omar.baldi.business@gmail.com>
2024-10-23 14:48:30 +0200

    Implemented logic to filtering out array entries based on debounced input value match and highlighted relevant chars

--- src/components/TypeAhead.tsx
+++ src/components/TypeAhead.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { useDebouncedValue } from "../hooks/useDebouncedValue";
 
 type TypeAheadData = {
@@ -25,16 +25,59 @@ export default function TypeAhead({
   const [inputV, setInputV] = useState("");
   const debouncedInputV = useDebouncedValue(inputV, debounce);
 
+  const typeaheadOptionsElements = useMemo<JSX.Element[]>(() => {
+    return data
+      .filter((opt) => opt.value.toLowerCase().match(debouncedInputV.toLowerCase()))
+      .map(({ label, value }) => {
+        const startIdx = label.indexOf(debouncedInputV);
+        const endIdx = startIdx + (debouncedInputV.length - 1);
+
+        const chars = [...label].map((char, charIdx) => ({
+          char,
+          isHighlighted: charIdx >= startIdx && charIdx <= endIdx,
+        }));
+
+        return {
+          chars,
+          value,
+        };
+      })
+      .map(({ chars, value }) => {
+        return (
+          <div key={undefined} style={{ textAlign: "start", margin: "1rem 0" }}>
+            {chars.map(({ char, isHighlighted }) => {
+              const charColor: React.CSSProperties["color"] = isHighlighted
+                ? "orange"
+                : "initial";
+
+              return (
+                <span
+                  key={undefined}
+                  style={{ color: charColor }}
+                  onClick={() => onOptionClick?.(value)}
+                >
+                  {char}
+                </span>
+              );
+            })}
+          </div>
+        );
+      });
+  }, [debouncedInputV, data, onOptionClick]);
+
   return (
     <>
       {label && <label htmlFor="typeahead-input">{label}</label>}
 
       <input
+        {...inputProps}
         type="text"
         name="typeahead-input"
         value={inputV}
         onChange={(e) => setInputV(e.target.value)}
       />
+
+      {typeaheadOptionsElements}
     </>
   );
 }
---
+++ src/data.ts
@@ -0,0 +1,10 @@
+export default [
+  "Tomato Basil Bruschetta",
+  "Chapli Kebabs",
+  "Korean Bibimbap",
+  "Turkish Kebabs",
+  "Blueberry Banana Smoothie",
+  "Lebanese Falafel Wrap",
+  "Room Name One",
+  "Room Name Two",
+];
