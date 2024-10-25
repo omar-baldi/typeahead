eb49013e9ad4588647b1b37d432d9c2766960408
Omar Baldi <omar.baldi.business@gmail.com>
2024-10-25 15:02:52 +0200

    Added test to assert only options that match the value searched are rendered on input element change

--- tests/unit/components/TypeAhead.test.tsx
+++ tests/unit/components/TypeAhead.test.tsx
@@ -1,9 +1,28 @@
 import TypeAhead from "@/components/TypeAhead";
 import data from "@/data";
-import { render } from "@testing-library/react";
+import { fireEvent, render, RenderResult } from "@testing-library/react";
+import { act } from "react";
 import { vi } from "vitest";
 
 vi.useFakeTimers();
+
+async function mockTypeaheadInputChangeEvent<W extends RenderResult>(
+  wrapper: W,
+  v: string,
+  timerToAdvance: number
+) {
+  const inputElement = await wrapper.getByTestId("typeahead-input");
+
+  act(() => {
+    fireEvent.change(inputElement, {
+      target: {
+        value: v,
+      },
+    });
+
+    vi.advanceTimersByTime(timerToAdvance);
+  });
+}
 
 describe("TypeAhead", () => {
   const typeaheadData = data.map((d) => ({ label: d, value: d }));
@@ -16,5 +35,21 @@ describe("TypeAhead", () => {
     const wrapper = render(<TypeAhead data={typeaheadData} />);
     const typeaheadOptions = await wrapper.getAllByTestId("typeahead-option");
     expect(typeaheadOptions).toHaveLength(9);
+  });
+
+  it("should render only typeahead options that match the value searched", async () => {
+    const debounceValue = 1000;
+    const wrapper = render(<TypeAhead data={typeaheadData} debounce={debounceValue} />);
+    await mockTypeaheadInputChangeEvent(wrapper, "oo", debounceValue);
+    const typeaheadOptions = await wrapper.getAllByTestId("typeahead-option");
+    expect(typeaheadOptions).toHaveLength(4);
+    expect(typeaheadOptions.map((t) => t.innerText)).toEqual(
+      expect.arrayContaining([
+        "Room Name One",
+        "Room Name Two",
+        "Room Name Oon",
+        "Blueberry Banana Smoothie",
+      ])
+    );
   });
 });
